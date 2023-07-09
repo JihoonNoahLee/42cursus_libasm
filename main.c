01b96211442d0ce64d3d65ef5e71519d4c6cb662
JihoonNoahLee <jihoolee@student.42SEOUL.kr>
2023-07-09 18:52:59 +0900

    [FEAT]: remove ft_strlen

--- main.c
+++ main.c
@@ -6,23 +6,12 @@
 /*   By: jihoolee <jihoolee@student.42SEOUL.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 23:06:39 by jihoolee          #+#    #+#             */
-/*   Updated: 2023/07/02 17:17:02 by jihoolee         ###   ########.fr       */
+/*   Updated: 2023/07/09 18:52:22 by jihoolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-/*
-size_t	ft_strlen(const char *str)
-{
-	size_t	len;
-
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
-}
-*/
 extern size_t	_ft_strlen(const char* s);
 
 int	main(void)
