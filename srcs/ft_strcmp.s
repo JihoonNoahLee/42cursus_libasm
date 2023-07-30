0af00d271b1e2859f89546ff1679dac7a85724ce
JihoonNoahLee <jihoolee@student.42SEOUL.kr>
2023-07-30 20:53:44 +0900

    [FIX]: ft_strcmp.s comments added

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -2,6 +2,7 @@ section .text
     global _ft_strcmp
 
 ;rdi: s1, rsi: s2
+;TODO: check about the 64bit implementation
 _ft_strcmp:
     mov rcx, 0
     jmp .compare_loop
@@ -10,7 +11,7 @@ _ft_strcmp:
         mov rax, [rdi + rcx]
         sub rax, [rsi + rcx]
         jmp .check
-        inc rcx
+        inc rcx                 ;inc 대신 4byte씩 check 하는걸로 정의할수도?
         jmp .compare_loop
 
     .check:
